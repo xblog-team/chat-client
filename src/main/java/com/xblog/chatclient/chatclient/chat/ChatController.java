fc1317c9763798c5d481e619e0b15fbc398f1dcc
강경훈 <kkh5535@naver.com>
2024-08-19 16:48:29 +0900

    init

---
+++ src/main/java/com/xblog/chatclient/chatclient/chat/ChatController.java
@@ -0,0 +1,14 @@
+package com.xblog.chatclient.chatclient.chat;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+	@GetMapping("/chat")
+	public String chat() {
+		return "chat";
+	}
+
+}
---
+++ src/test/java/com/xblog/chatclient/chatclient/ChatclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xblog.chatclient.chatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
